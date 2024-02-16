9f294cd6b5124f824545096766b2a170eb568b2c
owolcz <wolczoskar@gmail.com>
2024-02-16 16:35:55 +0100

    chrisp test message

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Chirp;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response; 
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Response 
     {
         //
+        return response('Hello, World!');
     }
 
     /**
